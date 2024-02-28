d0c16809388961103219cac52cdc02446a31c99e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-02-28 22:45:49 +0200

    black formatting basic files and adding simple url routes between apps

--- mus_mate/urls.py
+++ mus_mate/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("forum.urls", namespace="forum")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
