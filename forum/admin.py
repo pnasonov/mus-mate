d825a2ef36f8529e34847925d846319c24a08e55
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-02-29 13:01:17 +0200

    setting up admin panel to handle user, song, playlist, post, commentary instances

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from forum.models import User, Song, Playlist, Post, Commentary
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("about_me",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "email",
+                    "about_me",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("title", "artist", "owner")
+
+
+@admin.register(Playlist)
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner")
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "created_time")
+
+
+@admin.register(Commentary)
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ("content", "created_time", "user", "post")
