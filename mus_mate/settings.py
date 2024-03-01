d509fc423aa0c1480b4b71c9d0791c41c47c4471
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-01 10:44:42 +0200

    installing django-crispy-forms and crispy-bootstrap5

--- mus_mate/settings.py
+++ mus_mate/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "debug_toolbar",
     "accounts",
     "forum",
@@ -73,6 +75,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "mus_mate.wsgi.application"
 
