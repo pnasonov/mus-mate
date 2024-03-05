c1f1643929d83a7e669572f5ffbf1c2f1458106b
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-06 01:57:04 +0200

    add `musmate.onrender.com` to ALLOWED_HOSTS

--- mus_mate/settings.py
+++ mus_mate/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "musmate.onrender.com"]
 
 
 # Application definition
