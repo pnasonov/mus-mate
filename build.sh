b3fb99bf77a4bc0a2faba2784d236095be6af65e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2024-03-06 02:28:06 +0200

    build.sh error fix

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
